26cbe0fb98375a1b5ed6c6ea2af4c2c5cfc2be10
moemarusan <moemarusan@gmail.com>
2020-04-29 14:04:17 +0900

    :wrench:[fix] rootのtitle変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO/>
+      <SEO title=" "/>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
