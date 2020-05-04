a71847402708b07e95dc057f2d489d76453e12aa
moemarusan <moemarusan@gmail.com>
2020-05-05 05:29:18 +0900

    :wrench:[fix] summaryに戻した

--- src/components/seo.js
+++ src/components/seo.js
@@ -59,7 +59,7 @@ const SEO = ({ description, lang, meta, title }) => {
         },
         {
           name: `twitter:card`,
-          content: `summary_large_image`,
+          content: `summary`,
         },
         {
           name: `twitter:creator`,
