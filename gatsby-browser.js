a2ab80e7f67ac799f34ebbe023bda386bfc06c69
Moena Saiki <moemarusan@gmail.com>
2021-08-06 22:55:17 +0900

    :wrench:[fix] code highlightの修正

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,3 @@
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/styles/index.scss"
-
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,13 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              showLineNumbers: true,
+              inlineCodeMarker: null,
+            },
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
         ],
