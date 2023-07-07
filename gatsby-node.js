95b1c9e14ce081b0690cd4a685d0ed883158246c
Moena Saiki <moemarusan@gmail.com>
2023-07-07 22:49:37 +0900

    :wrench:[fix] slug & redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,12 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
+  const { createRedirect } = actions
 
   // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+
+  createRedirect({ fromPath: '/okinawa_2306/', toPath: '/okinawa-2306/', isPermanent: true })
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
