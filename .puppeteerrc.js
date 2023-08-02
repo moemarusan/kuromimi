72a2544bc120e3bdab8223f0e53e643a822b5e05
Moena Saiki <moemarusan@gmail.com>
2023-08-03 01:25:40 +0900

    [wip] test

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,6 +1,8 @@
+console.log('Puppeteer configuration loaded successfully');
+
 /**
  * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-    cacheDirectory: require("path").join(".", ".cache", "puppeteer"),
+    cacheDirectory: require("path").join(__dirname, ".cache", "puppeteer"),
 }
