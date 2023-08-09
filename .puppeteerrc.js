5d451484357999f3e9ab1f307714d5009cc57e05
Moena Saiki <moemarusan@gmail.com>
2023-08-09 23:33:40 +0900

    test

--- .puppeteerrc.js
+++ .puppeteerrc.js
@@ -1,8 +1,6 @@
-const {join} = require('path');
-
 /**
- * @type {import("puppeteer").Configuration}
+ * @type {import('puppeteer').Configuration}
  */
 module.exports = {
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
+    cacheDirectory: require("path").join(__dirname, ".cache", "puppeteer"),
+}
